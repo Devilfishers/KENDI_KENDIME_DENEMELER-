fec8c2aa036da064ba47cf5c92723ec882580cd1
Devilfishers <111433050+Devilfishers@users.noreply.github.com>
2022-10-05 11:13:00 +0300

    Update linefollower.py

--- linefollower.py
+++ linefollower.py
@@ -13,16 +13,16 @@ rg.setup(22,rg.OUT)  #Setting the GPIO22 pin as output
 ENA=rg.PWM(18,100)   #Setting the GPIO18 pin as ENA pin in order to control the motorA's speed values
 ENB=rg.PWM(22,100)   #Setting the GPIO22 pin as ENB pin in order to control the motorB's speed values
 
-rg.setup(23,rg.OUT)  #Setting the GPIO23 pin as output
-rg.setup(24,rg.OUT)  #Setting the GPIO24 pin as output
-rg.setup(17,rg.OUT)  #Setting the GPIO17 pin as output
-rg.setup(27,rg.OUT)  #Setting the GPIO27 pin as output
+rg.setup(23,rg.OUT)  #Setting the GPIO23 (IN1) pin as output
+rg.setup(24,rg.OUT)  #Setting the GPIO24 (IN2) pin as output
+rg.setup(17,rg.OUT)  #Setting the GPIO17 (IN3) pin as output
+rg.setup(27,rg.OUT)  #Setting the GPIO27 (IN4) pin as output
 
 ENB.start(0)        #setting the initial speed value as 0 for motorB
 ENA.start(0)        #setting the initial speed value as 0 for motorA
 
-rg.setwarnings(False)  #sometimes the system may needs to be shutdown instantly and when it does, the RPi.GPIO module may give some warning errors. This line turns off
-                     #those warnings 
+rg.setwarnings(False)  #sometimes the system may needs to be shutdown instantly and when it does, the RPi.GPIO module may give some warning errors. This line turns off...
+#...those warnings 
 
 I= int() #declaring an empty intiger variable for INTEGRAL segment
 K= int() #declaring an empty intiger variable for PROPORTIONAL segment
@@ -42,42 +42,53 @@ maxB=90   #declaring the max speed value of motorA
 lastError=0 #declaring an intiger for DERIVATIVE segment to use in order to calculate the error value the system will encounter
 
 def forward(pwmA,pwmB): #creating a function for setting the appropirate speed value for motorA and motorB so that the robot goes forward by various rotation angles
- rg.output(23 ,rg.LOW)
- rg.output(24, rg.HIGH)
- rg.output(27, rg.HIGH)
- rg.output(17, rg.LOW)
- ENA.ChangeDutyCycle(pwmA)
- ENB.ChangeDutyCycle(pwmB)
+  
+ rg.output(23 ,rg.LOW)  #setting the IN1 as LOW also sets OUT1 as LOW
+ rg.output(24, rg.HIGH) #setting the IN2 as HIGH also sets OUT2 as HIGH
+ rg.output(27, rg.HIGH) #setting the IN4 as HIGH also sets OUT4 as HIGH
+ rg.output(17, rg.LOW)  #setting the IN3 as LOW also sets OUT3 as LOW
+  
+ ENA.ChangeDutyCycle(pwmA) #setting ENA pin to a desired speed value by changing the pwmA parameter between 0 and 100
+ ENB.ChangeDutyCycle(pwmB) #setting ENB pin to a desired speed value by changing the pwmB parameter between 0 and 100
 
 
 
-def PID_control(offset): 
+def PID_control(offset): #creating a function that allows the robot to move autonomously
 
  global P  #setting the empty intiger P variable as global, in order for the PID_control function to recognize that variable
  global I  #setting the empty intiger I variable as global, in order for the PID_control function to recognize that variable
  global D  #setting the empty intiger D variable as global, in order for the PID_control function to recognize that variable
  global lastError #setting the empty intiger lastError variable as global in order for the PID_control function to recognize that variable
 
- error = 330- offset
- P= error
- I= I + error
- D= error - lastError
+ error = 330- offset #declaring and calculating an error value respect to an offset value. error can differ from 0 to 700
+
+ P= error #creating a PROPORTIONAL segment in order to make a speed value that is proportional to the error value by using P variable
+  
+ I= I + error #creating an INTEGRAL segment in order to make a speed value according to the STEADY-STATE error value by utilizing the summation of past error values...
+ #...with usage of I variable
+  
+ D= error - lastError #creating a DERIVATIVE segment in order to prevent overshooting by calculating the overcoming error values with usage of D variable
+  
  lastError=error
 
-
- movement= (Kp*P) + (Ki*I) + (Kd*D)
- speedA= baseA + movement
- speedB= baseB - movement
+ movement= (Kp*P) + (Ki*I) + (Kd*D) #calculating the correction of speed value since the process in the system is speed and actuators are motors
+  
+ speedA= baseA + movement #updating the speed value of motorA according to the distance of system's center
+ speedB= baseB - movement #updating the speed value of motorB according to the distance of system's center
 
  if speedA > maxA:
-     speedA = maxA
- if speedB > maxB:
-     speedB = maxB
- if speedA < 0:
-     speedA = 0
+     speedA = maxA  #this equation prevents the overshooting of speedA variable for motorA since motors can not read values that are more than 100
+    
+ if speedB > maxB: 
+     speedB = maxB  #this equationg prevents the overshooting of speedA variable for motorA since motors can not read values that are more than 100
+    
+ if speedA < 0:  
+     speedA = 0     #this equationg prevents the overshooting of speedA variable for motorA since motors can not read values that are less than 0
+    
  if speedB < 0:
-     speedB = 0
- forward(speedA,speedB)
+     speedB = 0     #this equationg prevents the overshooting of speedB variable for motorB since motors can not read values that are less than 0
+    
+ forward(speedA,speedB) #according to the correction of speed values; the "forward" function ,that is nested within the PID function, is called
 
 while True: #since a video is a colletcion of photos an infinite loop is necessary in order for the camera to repeatidely take some photos and view them as a video
     
